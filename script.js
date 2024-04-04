22ae3b6d9980f9a66b40a956a552bf49a12a1c9f
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-04-04 04:13:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
-const api_status = document.querySelector('.api-status');
-const database_status = document.querySelector('.database-status');
-const generate_button = document.querySelector('.generate-button');
+const websocket_status = document.querySelector('.websocket-status');
 const grid_container = document.querySelector('.grid-container');
 
-generate_button.disabled = true;
+let socket;
 
-async function check_api_status() {
-
-    const api_url = document.querySelector('.api-url').value;
+function connect_to_websocket() {
 
     try {
 
-        const response = await fetch(api_url);
+        var websocket_url = document.querySelector('.websocket-url').value;
 
-        const data = await response.text();
+        socket = new WebSocket(websocket_url);
 
-        if (data === 'flask') {
+        socket.onopen = function(event) {
 
-            api_status.innerHTML = `ONLINE | ${api_url} | ${new Date().toLocaleTimeString()}`;
+            websocket_status.innerHTML = `WebSocket connected. | ${websocket_url}`;
 
-            localStorage.setItem('api_url', api_url);
+        };
 
-        } else {
+        socket.onclose = function(event) {
 
-            api_status.innerHTML = 'Error: Unexpected response';
+            websocket_status.innerHTML = `WebSocket disconnected. | ${websocket_url}`;
 
-        }
+            setTimeout(connect_to_websocket, 3000);
 
-    } catch (error) {
-        
-        api_status.innerHTML = 'Error: ' + error.message;
+        };
 
-    }
+        socket.onerror = function(event) {
 
-}
+            websocket_status.innerHTML = `WebSocket error. | ${websocket_url}`;
 
-async function update_images() {
+            setTimeout(connect_to_websocket, 3000);
 
-    const api_url = document.querySelector('.api-url').value;
+        };
 
-    fetch(`${api_url}/images`)
+        socket.onmessage = function(event) {
 
-        .then(response => response.json())
-
-        .then(data => {
-
-            database_status.innerHTML = `Database updated. | ${new Date().toLocaleTimeString()}`;
+            data = JSON.parse(event.data);
 
             grid_container.innerHTML = '';
 
             data.forEach(image_url => {
 
-                const img = document.createElement('img');
+                var img = document.createElement('img');
 
                 img.src = image_url;
                 img.alt = 'image';
 
-                grid_container.insertBefore(img, grid_container.firstChild);
+                grid_container.appendChild(img);
 
             });
 
-        })
+        };
 
-        .catch(error => console.error('Error fetching data:', error));
+    } catch (error) {
+
+        setTimeout(connect_to_websocket, 3000);
+
+    } 
+}
+
+function send_message(message) {
+
+    if (typeof socket !== 'undefined' && socket.readyState === WebSocket.OPEN) {
+
+        socket.send(message);
+
+    } else {
+
+        console.log('WebSocket connection is not open right now.');
+
+    }
+}
+
+function update_images() {
+
+    send_message('update images');
 
 }
 
@@ -72,46 +84,17 @@ function generate_images() {
 
     if (prompt != '') {
 
-        const api_url = document.querySelector('.api-url').value;
+        send_message(`generate : ${prompt}`);
 
-        fetch(`${api_url}/generate?prompt=${prompt}`)
-    
-            .then(response => response.json())
-    
-            .then(data => {
-    
-                database_status.innerHTML = `Database updated. | ${new Date().toLocaleTimeString()}`;
-    
-                grid_container.innerHTML = '';
-    
-                data.forEach(image_url => {
-    
-                    const img = document.createElement('img');
-    
-                    img.src = image_url;
-                    img.alt = 'image';
-    
-                    grid_container.insertBefore(img, grid_container.firstChild);
-    
-                });
-    
-            })
-    
-            .catch(error => {
-                
-                console.error('Error fetching data:', error)
-                
-            });
+    }
 
-    } 
-    
 }
 
-generate_button.addEventListener('click', generate_images);
+connect_to_websocket();
 
-document.querySelector('.api-url').value = localStorage.getItem('api_url');
+setInterval(update_images, 3000);
 
-setInterval(check_api_status, 10000);
-setInterval(update_images, 10000);
+button = document.querySelector('.button');
 
-document.querySelector('.generate-button').disabled = true;
+button.addEventListener('click', generate_images);
+
